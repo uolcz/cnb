b238fc5eda2d83a0b359d2a0da9e6dbf6d3aceb6
Jan Zikan <zikan@uol.cz>
2014-02-25 17:25:25 +0100

    Removed class CNB. Moved its methods to module CNB.

--- lib/cnb.rb
+++ lib/cnb.rb
@@ -4,28 +4,28 @@ require_relative 'cnb/daily_cur_rates_parser'
 require_relative 'cnb/monthly_cur_rates_parser'
 
 module CNB
-  class CNB
-    def self.daily_rates
+  class << self
+    def daily_rates
       @parser = DailyCurRatesParser.new
     end
 
-    def self.monthly_rates(month, year)
+    def monthly_rates(month, year)
       @parser = MonthlyCurRatesParser.new(month, year)
     end
 
-    def self.date
+    def date
       @parser.date
     end
 
-    def self.rate(cur_code)
+    def rate(cur_code)
       @parser.rate(cur_code)
     end
 
-    def self.name(cur_code)
+    def name(cur_code)
       @parser.name(cur_code)
     end
 
-    def self.country(cur_code)
+    def country(cur_code)
       @parser.country(cur_code)
     end
   end
