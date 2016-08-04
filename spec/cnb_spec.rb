def19b7338308e2c271ba4de1efda93d57de2829
Boris Stitnicky <borisstitnicky@gmail.com>
2016-08-04 15:10:53 +0200

    Comply with style guide

--- spec/cnb_spec.rb
+++ spec/cnb_spec.rb
@@ -6,20 +6,20 @@ RSpec.describe CNB do
   end
 
   it 'has .daily_rate module method' do
-    spy = double( "CNB::DailyRates" )
-    allow( CNB::DailyRates ).to receive( :new ).and_return( spy )
-    expect( spy ).to receive( :rate_for ).with( :currency, :date )
+    spy = double 'CNB::DailyRates'
+    allow(CNB::DailyRates).to receive(:new).and_return spy
+    expect(spy).to receive(:rate_for).with :currency, :date
     CNB.daily_rate :currency, :date
-    expect( spy ).to receive( :rate_for ).with( :currency, Date.today )
+    expect(spy).to receive(:rate_for).with :currency, Date.today
     CNB.daily_rate :currency
   end
 
   it 'has .monthly_rate module method' do
-    spy = double( "CNB::MonthlyRates" )
-    allow( CNB::MonthlyRates ).to receive( :new ).and_return( spy )
-    expect( spy ).to receive( :rate_for ).with( :currency, :date )
+    spy = double 'CNB::MonthlyRates'
+    allow(CNB::MonthlyRates).to receive(:new).and_return spy
+    expect(spy).to receive(:rate_for).with :currency, :date
     CNB.monthly_rate :currency, :date
-    expect( spy ).to receive( :rate_for ).with( :currency, Date.today )
+    expect(spy).to receive(:rate_for).with :currency, Date.today
     CNB.monthly_rate :currency
   end
 end
