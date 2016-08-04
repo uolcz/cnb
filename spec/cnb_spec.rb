d478a64223251ccd59d2ee3cc6a8edd0fd5b03b7
Boris Stitnicky <borisstitnicky@gmail.com>
2016-08-04 13:01:21 +0200

    Increase coverage

--- spec/cnb_spec.rb
+++ spec/cnb_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe CNB do
   it 'has a version number' do
     expect(CNB::VERSION).not_to be nil
   end
+
+  it 'has .daily_rate module method' do
+    spy = double( "CNB::DailyRates" )
+    allow( CNB::DailyRates ).to receive( :new ).and_return( spy )
+    expect( spy ).to receive( :rate_for ).with( :currency, :date )
+    CNB.daily_rate :currency, :date
+    expect( spy ).to receive( :rate_for ).with( :currency, Date.today )
+    CNB.daily_rate :currency
+  end
+
+  it 'has .monthly_rate module method' do
+    spy = double( "CNB::MonthlyRates" )
+    allow( CNB::MonthlyRates ).to receive( :new ).and_return( spy )
+    expect( spy ).to receive( :rate_for ).with( :currency, :date )
+    CNB.monthly_rate :currency, :date
+    expect( spy ).to receive( :rate_for ).with( :currency, Date.today )
+    CNB.monthly_rate :currency
+  end
 end
